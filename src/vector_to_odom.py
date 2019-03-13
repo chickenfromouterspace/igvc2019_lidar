7e4d4c1b33a59badc11ac702f7677d6800888e40
Don Vo <mailforthedon@gmail.com>
2019-03-13 18:53:58 -0400

    Sending RPY transforms

--- src/vector_to_odom.py
+++ src/vector_to_odom.py
@@ -6,65 +6,81 @@ from math import sin, cos, pi
 import rospy
 import tf
 import tf2_ros
+import tf2_geometry_msgs
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
+from std_msgs.msg import Header
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3, Vector3Stamped
 
-rospy.init_node('odometry_publisher')
+class RPY:
+        def __init__(self, rpy_topic="imu/rpy"):
+            # Subscribe to the laser scan topic
+            rpy_sub = rospy.Subscriber(rpy_topic, Vector3Stamped, self.on_rpy)
 
-odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
-odom_broadcaster = tf.TransformBroadcaster()
+        def on_rpy(self, rpy):
+            self.vector = rpy.vector
+            # first, we'll publish the transform over tf
+            odom_broadcaster.sendTransform(
+                (self.vector.x, self.vector.y, 0.),
+                odom_quat,
+                current_time,
+                "base_link",
+                "odom"
+            )
+            print self.vector            
 
-x = 0.0
-y = 0.0
-th = 0.0
+if __name__ == '__main__':
 
-vx = 0.1
-vy = -0.1
-vth = 0.1
+    rospy.init_node('odometry_publisher')
 
-current_time = rospy.Time.now()
-last_time = rospy.Time.now()
+    odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
+    odom_broadcaster = tf.TransformBroadcaster()
+    rpy = RPY("imu/rpy")
 
-r = rospy.Rate(1.0)
-while not rospy.is_shutdown():
+    x = 0.0
+    y = 0.0
+    th = 0.0
+
+    vx = 0.1
+    vy = -0.1
+    vth = 0.1
+
     current_time = rospy.Time.now()
+    last_time = rospy.Time.now()
 
-    # compute odometry in a typical way given the velocities of the robot
-    dt = (current_time - last_time).to_sec()
-    delta_x = (vx * cos(th) - vy * sin(th)) * dt
-    delta_y = (vx * sin(th) + vy * cos(th)) * dt
-    delta_th = vth * dt
+    r = rospy.Rate(1.0)
+    while not rospy.is_shutdown():
+        current_time = rospy.Time.now()
 
-    x += delta_x
-    y += delta_y
-    th += delta_th
+        # compute odometry in a typical way given the velocities of the robot
+        dt = (current_time - last_time).to_sec()
+        delta_x = (vx * cos(th) - vy * sin(th)) * dt
+        delta_y = (vx * sin(th) + vy * cos(th)) * dt
+        delta_th = vth * dt
 
-    # since all odometry is 6DOF we'll need a quaternion created from yaw
-    odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
+        x += delta_x
+        y += delta_y
+        th += delta_th
 
-    # first, we'll publish the transform over tf
-    odom_broadcaster.sendTransform(
-        (x, y, 0.),
-        odom_quat,
-        current_time,
-        "base_link",
-        "odom"
-    )
+        # since all odometry is 6DOF we'll need a quaternion created from yaw
+        odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
 
-    # next, we'll publish the odometry message over ROS
-    odom = Odometry()
-    odom.header.stamp = current_time
-    odom.header.frame_id = "odom"
 
-    # set the position
-    odom.pose.pose = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
+        # next, we'll publish the odometry message over ROS
+        odom = Odometry()
+        odom.header.stamp = current_time
+        odom.header.frame_id = "odom"
 
-    # set the velocity
-    odom.child_frame_id = "base_link"
-    odom.twist.twist = Twist(Vector3(vx, vy, 0), Vector3(0, 0, vth))
+        # set the position
+        odom.pose.pose = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
 
-    # publish the message
-    odom_pub.publish(odom)
+        # set the velocity
+        odom.child_frame_id = "base_link"
+        odom.twist.twist = Twist(Vector3(vx, vy, 0), Vector3(0, 0, vth))
 
-    last_time = current_time
-    r.sleep()
+        # publish the message
+        odom_pub.publish(odom)
+
+        last_time = current_time
+        r.sleep()
+
+    rospy.spin()
